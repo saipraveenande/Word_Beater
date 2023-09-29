2710b1dde678715f450f81a00d5571f9c4790dcc
Sai Praveen Ande <87641528+saipraveenande@users.noreply.github.com>
2023-09-29 22:30:56 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,38 +1,42 @@
-window.addEventListener('load', init);
+window.addEventListener('load', function(){
+    this.setTimeout(function open(event){
+        document.querySelector(".pop-up").style.display="block";
+    }, 1000)
+});
 
-var levels = {
-    easy:6,
-    medium: 4,
-    hard: 2,
-}
-let currentLevel=levels.medium;
+var levels = {easy:6, medium: 4, hard: 2 }
+var currentLevel;
+var time;
+
 const buttons = document.querySelectorAll('button');
 buttons.forEach(button =>{
     button.addEventListener('click', ()=>{
         buttons.forEach(btn => btn.classList.remove('active'));
         button.classList.add('active');
+        document.querySelector(".pop-up").style.display="none";
+        init();
         switch (button.id){
             case 'easy':
                 currentLevel=levels.easy;
+                time=levels.easy;
                 seconds.innerHTML = currentLevel;
                 break;
             case 'medium':
                 currentLevel = levels.medium;
+                time = levels.medium;
                 seconds.innerHTML = currentLevel;
                 break;
             case 'hard':
                 currentLevel = levels.hard;
+                time = levels.hard;
                 seconds.innerHTML = currentLevel;
                 break;
         }
     });
 });
 
-
-let time = currentLevel;
 let score = 0;
 let isPlaying;
-
 
 const wordInput = document.querySelector("#wordInput");
 const currentWord = document.querySelector("#currentWord");
@@ -41,16 +45,14 @@ const timeDisplay = document.querySelector("#time");
 const message = document.querySelector("#message");
 const seconds = document.querySelector("#seconds");
 
-
 const word = ['hat', 'river', 'javascript', 'sai', 'praveen', 'sai praveen', 'lucky', 'statue', "giraffe", "rainbow", "tornado", "kite", "whale", "banana", "vase", "pencil", "jellyfish", "lighthouse",
-    "elephant", "dolphin", "quilt", "watermelon","xylophone", "zebra", "fireworks", "quokka", "jacket", "dog", "umbrella", "octopus", "monkey", "flower",
-    "helicopter", "orange", "mountain", "ice cream", "airplane", "notebook", "grape", "tiger", "carrot", "candle", "noodle", "lemon", "umbrella", "elephant",
-    "xylophone", "sunglasses", "apple", "penguin", "zebra", "xylophone", "volcano", "kangaroo", "rabbit", "honey" ];
-
+    "elephant", "dolphin", "quilt", "watermelon","xylophone", "zebra", "fireworks", "quokka", "jacket", "dog", "octopus", "monkey", "flower",
+    "helicopter", "orange", "mountain", "ice cream", "airplane", "notebook", "grape", "tiger", "carrot", "candle", "noodle", "lemon", "umbrella",
+    "sunglasses", "apple", "penguin", "zebra", "volcano", "kangaroo", "rabbit", "honey" ];
 
 function init(){
-    randomWord(word);
     wordInput.addEventListener('input', startMatch);
+    randomWord(word);
     setInterval(countdown, 1000);
     setInterval(checkStatus, 100);
 }
@@ -69,23 +71,21 @@ function startMatch(){
         scoreDisplay.innerHTML = score;
     }
 }
-
 function matchWord(){
     if(wordInput.value === currentWord.innerHTML){
         message.innerHTML = 'currect!';
         return true;
     } else {
-        message.innerHTML = " ";
+        message.innerHTML = "";
         return false;
     }
 }
-
 randomWord=(word)=>{
     const randomIndex = Math.floor(Math.random() * word.length);
     currentWord.innerHTML = word[randomIndex];
 }
 countdown = ()=>{
-    if(time>0){
+    if((time>0) &&(isPlaying)){
         time--;
     }else if(time === 0 ){
         isPlaying=false;
@@ -94,8 +94,7 @@ countdown = ()=>{
 }
 function checkStatus(){
     if(!isPlaying && time === 0){
-        message.innerHTML="Game Over!";
+        message.innerHTML="Game Over! ";
         score = -1;
     }
 }
-
